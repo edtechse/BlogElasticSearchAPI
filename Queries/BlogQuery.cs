2b6d5fd1f6d1b5292737ce04876e9f1071e61ae0
edtechse <90820353+edtechse@users.noreply.github.com>
2021-10-14 11:11:54 +0530

    Update BlogQuery.cs

--- Queries/BlogQuery.cs
+++ Queries/BlogQuery.cs
@@ -9,17 +9,17 @@
 
         public static string SearchBlogQuery(string text, int page)
         {
-           return  @"{  ""size"":10,""from"":" + page + @",""query"":{""bool"":{""must"":[{""match"": {""blogtext.S"" :""" + text + @"""}}]}},""_source"":[""id"",""blogauthor.S"",""blogtitle.S"",""blogtext.S"",""blogcreationdate.S"",""likescount.S"",""rating.S""]}";
+           return  @"{  ""size"":10,""from"":" + page + @",""query"":{""bool"":{""must"":[{""match"": {""blogtext.S"" :""" + text + @"""}}]}},""_source"":[""blogid.S"",""blogauthor.S"",""blogtitle.S"",""blogtext.S"",""blogcreationdate.S"",""likescount.N"",""rating.N""]}";
         }
 
         public static string TrendingBlogQuery()
         {
-            return @"{  ""size"":1,""query"":{""bool"":{""filter"":[{""range"": {""blogcreationdate.S"":{""gte"": ""now-1M/M"", ""lte"":""now+5M/M""}}}]}},""_source"":[""id""],""aggs"":{""Trendings"":{""terms"":{""field"":""blogtags.SS.keyword"",""order"":{""_count"":""desc""},""size"":10}}}}";
+            return @"{  ""size"":1,""query"":{""bool"":{""filter"":[{""range"": {""blogcreationdate.S"":{""gte"": ""now-1M/M"", ""lte"":""now+5M/M""}}}]}},""_source"":[""blogid.S""],""aggs"":{""Trendings"":{""terms"":{""field"":""blogtags.SS.keyword"",""order"":{""_count"":""desc""},""size"":10}}}}";
         }
 
         public static string PersonalizedFeedQuery(string tags)
         {
-            return @"{  ""size"":10,""query"":{""bool"":{""must"":[{""match"": {""blogtags.SS"" :""" + tags + @"""}}]}},""sort"": [{""blogcreationdate.S"": {""order"": ""desc""}}],""_source"":[""id"",""blogauthor.S"",""blogtitle.S"",""blogtext.S"",""blogcreationdate.S"",""blogtags.SS"",""likescount.S"",""rating.S""]}";
+            return @"{  ""size"":10,""query"":{""bool"":{""must"":[{""match"": {""blogtags.SS"" :""" + tags + @"""}}]}},""sort"": [{""blogcreationdate.S"": {""order"": ""desc""}}],""_source"":[""blogid.S"",""blogauthor.S"",""blogtitle.S"",""blogtext.S"",""blogcreationdate.S"",""blogtags.SS"",""likescount.N"",""rating.N""]}";
         }
     }
 }
