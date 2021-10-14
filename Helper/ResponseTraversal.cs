78e3b1c67ae3dc16d52a22e811918291ec583b94
edtechse <90820353+edtechse@users.noreply.github.com>
2021-10-14 11:13:08 +0530

    Update ResponseTraversal.cs

--- Helper/ResponseTraversal.cs
+++ Helper/ResponseTraversal.cs
@@ -11,26 +11,26 @@ namespace BlogElasticSearchService.Configuration
         public Blog SearchResponse(JToken responseItem)
         {
              Blog _blog = new Blog();
-            _blog.Id = responseItem["_source"]["id"]["S"].ToString();
+            _blog.Id = responseItem["_source"]["blogid"]["S"].ToString();
             _blog.BlogAuthor = responseItem["_source"]["blogauthor"]["S"].ToString();
             _blog.BlogText = responseItem["_source"]["blogtext"]["S"].ToString();
             _blog.BlogTitle = responseItem["_source"]["blogtitle"]["S"].ToString();
             _blog.BlogCreationDate = responseItem["_source"]["blogcreationdate"]["S"].ToString();
-            _blog.LikesCount = responseItem["_source"]["likescount"]["S"].ToString();
-            _blog.Rating = responseItem["_source"]["rating"]["S"].ToString();
+            _blog.LikesCount = responseItem["_source"]["likescount"]["N"].ToString();
+            _blog.Rating = responseItem["_source"]["rating"]["N"].ToString();
             return _blog;
         }
 
         public Blog PersonalizedFeedResponse(JToken responseItem)
         {
             Blog _blog = new Blog();
-            _blog.Id = responseItem["_source"]["id"]["S"].ToString();
+            _blog.Id = responseItem["_source"]["blogid"]["S"].ToString();
             _blog.BlogAuthor = responseItem["_source"]["blogauthor"]["S"].ToString();
             _blog.BlogText = responseItem["_source"]["blogtext"]["S"].ToString();
             _blog.BlogTitle = responseItem["_source"]["blogtitle"]["S"].ToString();
             _blog.BlogCreationDate = responseItem["_source"]["blogcreationdate"]["S"].ToString();
-            _blog.LikesCount = responseItem["_source"]["likescount"]["S"].ToString();
-            _blog.Rating = responseItem["_source"]["rating"]["S"].ToString();
+            _blog.LikesCount = responseItem["_source"]["likescount"]["N"].ToString();
+            _blog.Rating = responseItem["_source"]["rating"]["N"].ToString();
 
             var TagList = JsonConvert.DeserializeObject<List<string>>(responseItem["_source"]["blogtags"]["SS"].ToString());
             _blog.BlogTags = String.Join(",", TagList);
